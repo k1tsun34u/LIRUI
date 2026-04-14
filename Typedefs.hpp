39eaf7352fb57fd41b36129da4d5b8acb358db5a
K1tsun34u <k1tsun34@gmail.com>
2026-04-14 17:53:50 +0300

    Added version macros

--- Typedefs.hpp
+++ Typedefs.hpp
@@ -16,6 +16,13 @@
 name='Microsoft.Windows.Common-Controls' version='6.0.0.0' \
 processorArchitecture='*' publicKeyToken='6595b64144ccf1df' language='*'\"")
 
+#define LIRUI_VERSION				"0.1.1-alpha"
+#define LIRUI_VERSION_MAJOR			0
+#define LIRUI_VERSION_MINOR			1
+#define LIRUI_VERSION_PATCH			1
+#define LIRUI_VERSION_TWEAK			0x0a			// 0x0a - alpha, 0x0b - beta, 0x0c - release candidate, 0x0f - release
+#define LIRUI_VERSION_INT			((LIRUI_VERSION_MAJOR << 24) | (LIRUI_VERSION_MINOR << 16) | (LIRUI_VERSION_PATCH << 8) | LIRUI_VERSION_TWEAK)
+
 #define WM_WAKEUP					(WM_APP + 1)
 
 namespace LIR {
